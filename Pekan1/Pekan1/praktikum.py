d5f10c65e4302dc98d54d06b939768f6f6046835
irraw <irwwanobody@gmail.com>
2026-09-08 18:41:14 +0700

    if mantap

---
+++ Pekan1/Pekan1/praktikum.py
@@ -0,0 +1,12 @@
+# Program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print ("hello world")
+print ()
+print ("print ini menghasilkan")
+print("empat baris output")
+print ("/ \\ // \\\\ /// \\\\\ " )
+print ("program ini" + "dibuat oleh a\n" + "shaira 2611537001")
